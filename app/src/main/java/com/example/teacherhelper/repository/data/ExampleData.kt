e46a2020353820e3968deb8ef6e83a9134a27ea5
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-07-22 14:08:17 +0500

    Add example data

---
+++ app/src/main/java/com/example/teacherhelper/repository/data/ExampleData.kt
@@ -0,0 +1,19 @@
+package com.example.teacherhelper.repository.data
+
+/**
+ * Пример группы
+ */
+class ExampleData {
+    private val student1: Student = Student(0, "Вадя","Двоечник")
+    private val student2: Student = Student(1, "Настя","Отличница")
+    val groups: List<Group> = listOf(Group(0,
+        "ЕГЭ Информатика",
+        listOf(student1,student2),
+        "ЕГЭшники"),
+        (
+                Group(1,
+            "ОГЭ Информатика",
+            listOf(student1,student2),
+            "ОГЭшники"))
+    )
+}
