3299561dd34ad1b5ceaee235ae21c25efa3e2559
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-07-26 16:28:07 +0500

    Detail Screen

---
+++ app/src/main/java/com/example/teacherhelper/view/compose/GroupDetailScreen.kt
@@ -0,0 +1,14 @@
+package com.example.teacherhelper.view.compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(showBackground = true)
+@Composable
+fun GroupDetailScreen(){
+    Box {
+        Text(text = "Hello World")
+    }
+}
