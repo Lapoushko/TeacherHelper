aeb409c3719aa269fb2417e8e4d69465231dc7e8
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-07-31 21:17:37 +0500

    Add condition

--- app/src/main/java/com/example/teacherhelper/presenter/MainActivityViewModel.kt
+++ app/src/main/java/com/example/teacherhelper/presenter/MainActivityViewModel.kt
@@ -5,46 +5,45 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.teacherhelper.repository.GroupsRepository
 import com.example.teacherhelper.repository.GroupsRepositoryImpl
 import com.example.teacherhelper.repository.data.Group
+import com.example.teacherhelper.util.Constants
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-
-private const val LOG_TAG = "ViewM"
 
 /**
  * ViewModel для MainScreen
  */
 @HiltViewModel
 class MainActivityViewModel @Inject constructor(): ViewModel() {
-    private val groupsRepository: GroupsRepository = GroupsRepositoryImpl()
 
+    @Inject lateinit var groupsRepository: GroupsRepositoryImpl
     private val resultMutableGroups = MutableLiveData<List<Group>>()
     val resultLiveGroups: LiveData<List<Group>> = resultMutableGroups
 
     init {
-        loadGroups()
-        Log.e(LOG_TAG,"vm created")
+        Log.e(Constants.LOG_TAG,"vm created")
     }
 
     override fun onCleared() {
-        Log.e(LOG_TAG,"vm cleared")
+        Log.e(Constants.LOG_TAG,"vm cleared")
         super.onCleared()
     }
 
     /**
      * Загрузка групп
      */
-    private fun loadGroups(){
+    fun loadGroups(){
         viewModelScope.launch {
-            val groups = groupsRepository.getGroups()
-            if (groups.isNotEmpty()){
-                Log.e(LOG_TAG, "vm load groups with count: ${groups.count()}")
-                resultMutableGroups.value = groups
-            } else{
-                Log.e(LOG_TAG, "vm is empty")
+            if (resultMutableGroups.value.isNullOrEmpty()) {
+                val groups = groupsRepository.getGroups()
+                if (groups.isNotEmpty()) {
+                    Log.e(Constants.LOG_TAG, "vm load groups with count: ${groups.count()}")
+                    resultMutableGroups.value = groups
+                } else {
+                    Log.e(Constants.LOG_TAG, "vm is empty")
+                }
             }
         }
     }
