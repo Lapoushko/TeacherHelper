01652de6c7231e58e15f84824009eb5a8b8a0020
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-07-31 21:16:59 +0500

    Refactoring

--- app/src/main/java/com/example/teacherhelper/view/compose/MainScreen.kt
+++ app/src/main/java/com/example/teacherhelper/view/compose/MainScreen.kt
@@ -7,28 +7,28 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.teacherhelper.presenter.MainActivityViewModel
+import com.example.teacherhelper.repository.data.Group
 
 /**
  * Контент основной активности
  */
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-//TODO Изменить viewModel() на hiltViewModel()
-fun MainScreen(mainActivityViewModel: MainActivityViewModel = viewModel(),
-               onClick : () -> Unit){
+fun MainScreen(mainActivityViewModel: MainActivityViewModel = hiltViewModel(),
+               onClick : (Group) -> Unit){
+    mainActivityViewModel.loadGroups()
     Scaffold(
         content = {
             LazyColumn(
                 contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
             ) {
                 items(
-                    items = mainActivityViewModel.resultLiveGroups.value!!,
+                    items = mainActivityViewModel.resultLiveGroups.value ?: emptyList(),
                     itemContent = {
-                        GroupListItem(group = it){
-                            onClick()
-                        }
+                        GroupListItem(group = it,
+                            onClick = onClick)
                     }
                 )
             }
