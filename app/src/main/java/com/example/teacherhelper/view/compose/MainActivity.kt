5626bdd7be4f7286af6fd4cb5dbc440f6791a789
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-07-31 21:18:23 +0500

    delete commentary

--- app/src/main/java/com/example/teacherhelper/view/compose/MainActivity.kt
+++ app/src/main/java/com/example/teacherhelper/view/compose/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.teacherhelper.view.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import com.example.teacherhelper.app.TeacherHelperApp
-import com.example.teacherhelper.presenter.MainActivityViewModel
 import com.example.teacherhelper.ui.theme.TeacherHelperTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,19 +12,15 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private val vm: MainActivityViewModel by viewModels<MainActivityViewModel>()
+
+//    private val mainViewModel: MainActivityViewModel by viewModels<MainActivityViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
-//        var groups = listOf<Group>()
-//        vm.resultLiveGroups.observe(this){
-//            groups = it
-//        }
+//        Log.e("ViewM", mainViewModel.toString())
         setContent {
             TeacherHelperTheme {
                 TeacherHelperApp().TeacherHelperSet()
-//                SurfaceCreator(groups = groups)
             }
         }
     }
