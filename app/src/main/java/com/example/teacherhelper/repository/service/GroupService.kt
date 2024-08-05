85fce03ac45e48928fc72650ec95dc2bdd01b872
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-08-05 16:01:25 +0500

    new functions in service

--- app/src/main/java/com/example/teacherhelper/repository/service/GroupService.kt
+++ app/src/main/java/com/example/teacherhelper/repository/service/GroupService.kt
@@ -1,6 +1,7 @@
 package com.example.teacherhelper.repository.service
 
 import com.example.teacherhelper.repository.data.Group
+import com.example.teacherhelper.repository.data.Student
 
 /**
  * Сервис групп
@@ -15,4 +16,19 @@ interface GroupService {
      * Получить все группы
      */
     fun getGroups(): List<Group>
+
+    /**
+     * Сохранить группу
+     */
+    fun saveGroup(group: Group)
+
+    /**
+     * Добавить студента в группу
+     */
+    fun addStudent(group: Group, student: Student)
+
+    /**
+     * Добавить новую группу
+     */
+    fun addGroup(group: Group)
 }
--- app/src/main/java/com/example/teacherhelper/repository/service/GroupServiceImpl.kt
+++ app/src/main/java/com/example/teacherhelper/repository/service/GroupServiceImpl.kt
@@ -2,6 +2,7 @@ package com.example.teacherhelper.repository.service
 
 import com.example.teacherhelper.repository.dao.GroupDao
 import com.example.teacherhelper.repository.data.Group
+import com.example.teacherhelper.repository.data.Student
 import javax.inject.Inject
 
 /**
@@ -17,5 +18,19 @@ class GroupServiceImpl @Inject constructor(dao: GroupDao) : GroupService {
 
     override fun getGroups(): List<Group> {
         return groupDao.getGroups()
+    }
+
+    override fun saveGroup(group: Group) {
+        groupDao.saveGroup(group)
+    }
+
+    override fun addStudent(group: Group, student: Student) {
+        val newStudents = group.students.toMutableList().apply { add(student) }
+        val newGroup = Group(group.id, group.name, newStudents, group.description)
+        saveGroup(newGroup)
+    }
+
+    override fun addGroup(group: Group) {
+        groupDao.addGroup(group)
     }
 }
