bfcb6a388376aae8e2973d4c0ecb4a51830517e4
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-08-21 22:28:58 +0500

    add relation

--- app/src/main/java/com/example/teacherhelper/repository/data/Group.kt
+++ app/src/main/java/com/example/teacherhelper/repository/data/Group.kt
@@ -1,5 +1,10 @@
 package com.example.teacherhelper.repository.data
 
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import com.example.teacherhelper.util.Constants
 import javax.inject.Inject
 
 /**
@@ -10,9 +15,38 @@ import javax.inject.Inject
  * @property students список студентов
  * @property description описание группы
  */
+@Entity(tableName = Constants.NAME_TABLE_GROUPS)
 data class Group @Inject constructor(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val name: String,
-    val students: List<Student>,
     val description: String
+)
+
+/**
+ * Студента
+ *
+ * @property studentId id ученика
+ * @property name имя ученика
+ * @property description описание ученика
+ * @author lapoushko
+ */
+@Entity
+data class Student @Inject constructor(
+    @PrimaryKey(autoGenerate = true) val studentId: Int,
+    val name: String,
+    val description: String,
+    val groupId: Int
+)
+
+/**
+ * Связь один-к-многим
+ */
+data class StudentByGroup(
+    @Embedded val group: Group,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "groupId"
+    )
+    val students: List<Student>
 )
