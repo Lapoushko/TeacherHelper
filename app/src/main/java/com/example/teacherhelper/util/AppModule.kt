ea2edf072dc8a2498ffd6cffa27a38ede81bc6df
Lapoushko <45125347+Lapoushko@users.noreply.github.com>
2024-08-21 22:27:51 +0500

    app module

---
+++ app/src/main/java/com/example/teacherhelper/util/AppModule.kt
@@ -0,0 +1,29 @@
+package com.example.teacherhelper.util
+
+import android.content.Context
+import androidx.room.Room
+import com.example.teacherhelper.repository.dao.Dao
+import com.example.teacherhelper.repository.data.GroupDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule{
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context : Context) =
+        Room.databaseBuilder(context, GroupDatabase::class.java, Constants.NAME_DATABASE)
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideDAO(appDatabase: GroupDatabase): Dao {
+        return appDatabase.dao()
+    }
+}
